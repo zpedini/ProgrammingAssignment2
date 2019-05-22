e742e9c1945e21a63417928bac0f5dba86410f16
zpedini <50925786+zpedini@users.noreply.github.com>
2019-05-22 14:15:36 -0400

    Add files via upload

---
+++ Peer_Graded_Assignment.R
@@ -0,0 +1,33 @@
+#function1
+makeCacheMatrix <- function(x = matrix()) {
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+
+#function2
+cacheSolve <- function(x, ...) {
+    i <- x$getinverse()
+    if (!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
+}
+
+#test
+m<- matrix(c(1,0,1,2),2,2)
+cm <- makeCacheMatrix(m)
+cacheSolve(cm)
